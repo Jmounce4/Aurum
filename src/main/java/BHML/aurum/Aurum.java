5995c89795c9762886748b5a298d20e66bd05d6b
Jmounce4 <jmounce4@uncc.edu>
2025-11-25 17:17:12 -0500

    Initial commit

---
+++ src/main/java/BHML/aurum/Aurum.java
@@ -0,0 +1,17 @@
+package BHML.aurum;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Aurum extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
